01a6d924d999a3d78d25893addf9ac7da565865d
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-09 13:05:36 -0400

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,22 @@
-// Write your "actions" router here!
+const express = require('express');
+const Actions = require('./actions-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+
+});
+router.get('/:id', async (req, res, next) => {
+
+});
+router.post('/', async (req, res, next) => {
+
+});
+router.put('/:id', async (req, res, next) => {
+
+});
+router.delete('/:id', async (req, res, next) => {
+
+});
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,29 @@
-// Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model');
+const Actions = require('../actions/actions-model');
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+
+})
+router.get('/:id', async (req, res, next) => {
+
+})
+router.post('/', async (req, res, next) => {
+
+})
+router.put('/:id', async (req, res, next) => {
+
+})
+router.delete('/:id', async (req, res, next) => {
+
+})
+router.get('/:id/actions', async (req, res, next) => {
+
+})
+
+module.exports = router
+
+
+
